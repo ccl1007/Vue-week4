303c4a91f2a51c9d80855a8bafc0bf151db6e709
ccl1007 <luku612150@gmail.com>
2022-03-04 01:13:03 +0800

    (參考範例模板)

---
+++ js/products.js
@@ -0,0 +1,163 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+
+let productModal = null;
+let delProductModal = null;
+
+const app =createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'luku612150',
+      products: [],
+      tempProduct: {
+        imagesUrl: [],
+      },
+      pagination: {},
+      isNew: false,
+    }
+  },
+  mounted() {
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    axios.defaults.headers.common.Authorization = token;
+    this.checkAdmin();
+  },
+  methods: {
+    checkAdmin() {
+      const url = `${this.apiUrl}/api/user/check`;
+      axios.post(url)
+        .then(() => {
+          this.getData();
+        })
+        .catch((err) => {
+          alert(err.data.message)
+          window.location = 'index.html';
+        })
+    },
+    getData(page = 1) {//參數預設值
+    // 1-1 修改api(products 包含分頁)
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products?page=${page}`;//query 參數
+
+      axios.get(url)
+        .then((response) => {
+          const { products, pagination } = response.data;
+          this.products = products;
+          this.pagination = pagination;
+        }).catch((err) => {
+          alert(err.data.message);
+          window.location = 'index.html';
+        })
+    },
+    openModal(isNew, item) {
+      if (isNew === 'new') {
+        this.tempProduct = {
+          imagesUrl: [],
+        };
+        this.isNew = true;
+        productModal.show();
+      } else if (isNew === 'edit') {
+        this.tempProduct = { ...item };
+        this.isNew = false;
+        productModal.show();
+      } else if (isNew === 'delete') {
+        this.tempProduct = { ...item };
+        delProductModal.show()
+      }
+    },
+  },
+});
+
+// 分頁元件
+app.component('pagination', {
+  template: '#pagination',
+// 1-建立props去接收pages
+  props: ['pages'],
+  
+  methods: {
+    emitPages(page) {
+      this.$emit('emit-pages', page);
+    },
+  },
+});
+
+// 產品新增/編輯元件
+app.component('productModal', {
+  template: '#productModal',
+  props: ['product', 'isNew'],
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'luku612150',
+    };
+  },
+  mounted() {
+    productModal = new bootstrap.Modal(document.getElementById('productModal'), {
+      keyboard: false,
+      backdrop: 'static'
+    });
+  },
+  methods: {
+    updateProduct() {
+      // 新增商品
+      let api = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
+      let httpMethod = 'post';
+      // 當不是新增商品時則切換成編輯商品 API
+      if (!this.isNew) {
+        api = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.product.id}`;
+        httpMethod = 'put';
+      }
+
+      axios[httpMethod](api, { data: this.product }).then((response) => {
+        alert(response.data.message);
+        this.hideModal();
+        this.$emit('update');
+      }).catch((error) => {
+        alert(error.data.message);
+      });
+    },
+    createImages() {
+      this.product.imagesUrl = [];
+      this.product.imagesUrl.push('');
+    },
+    openModal() {
+      productModal.show();
+    },
+    hideModal() {
+      productModal.hide();
+    },
+  },
+})
+// 產品刪除元件
+app.component('delProductModal', {
+  template: '#delProductModal',
+  props: ['item'],
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'hexschoolvue',
+    };
+  },
+  mounted() {
+    delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+      keyboard: false,
+      backdrop: 'static',
+    });
+  },
+  methods: {
+    delProduct() {
+      axios.delete(`${this.apiUrl}/api/${this.apiPath}/admin/product/${this.item.id}`).then((response) => {
+        this.hideModal();
+        this.$emit('update');
+      }).catch((error) => {
+        alert(error.data.message);
+      });
+    },
+    openModal() {
+      delProductModal.show();
+    },
+    hideModal() {
+      delProductModal.hide();
+    },
+  },
+});
+
+app.mount('#app');
